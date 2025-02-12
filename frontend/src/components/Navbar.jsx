8ac012bc560af2e97e0a47f571761176239a12aa
Krish0215 <krishnpal.23jiaiml041@jietjodhpur.ac.in>
2025-02-12 23:33:40 +0530

    navbar added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-white shadow-md py-4">
+      <div className="container mx-auto flex justify-center">
+        <h1 className="text-xl font-bold text-gray-800">EGO clothing</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
